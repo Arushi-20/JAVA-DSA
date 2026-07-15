a5d1b0c065b56c2431c3edb0290a1526adeb431d
Arushi Sharma <avpsharma3012@gmail.com>
2026-07-15 22:41:31 +0530

    LCM.java

--- LCM.java
+++ LCM.java
@@ -5,12 +5,14 @@ public class LCM {
         for (int i = 1; i <=10; i++) {
             System.out.print((i*num1)+" ");
         }
+        
         System.out.println(" ");
         int num2=6;
          System.out.print("Multiple of 6 are:");
         for (int i = 1; i <=10; i++) {    
             System.out.print((i*num2)+" ");
         }
+        
     System.out.println(" ");
     int lcm = (num1 * num2) / gcd(num1, num2);
         System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
