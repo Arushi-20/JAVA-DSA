07644619d9f973a156997549e30ff22d6bdb9054
Arushi Sharma <avpsharma3012@gmail.com>
2026-08-07 23:27:07 +0530

    String

---
+++ StringsBasics.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StringsBasics {
+    public static void main(String[] args) {
+        char[] arr={'a','r','u','s','h','i'};
+        // for(char ele: arr){
+        //     System.out.print(ele+" ");
+        // }
+        // String x= "Arushi";
+        // System.out.println(x);
+        // Scanner sc = new Scanner(System.in);
+        // //when we enter space after one word then they does not print after the one word in next() case
+        // // String s= sc.next();
+        // String s= sc.nextLine();
+        // System.out.println(s);
+
+        String s= "Arushi Sharma";
+        System.out.println(s.charAt(3));
+
+        System.out.println(arr[2]);
+
+    }
+}
