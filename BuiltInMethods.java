20257e07efc127bae3c483e0ba41b5fb37a990c7
Arushi Sharma <avpsharma3012@gmail.com>
2026-08-08 10:59:20 +0530

    Built-in methods of String

---
+++ BuiltInMethods.java
@@ -0,0 +1,22 @@
+public class BuiltInMethods {
+    public static void main(String[] args) {
+        String s="Arushi Sharma";
+       
+        System.out.println(s.indexOf('a'));
+
+        System.out.println(s.lastIndexOf('a'));
+        
+        System.out.println(s.toLowerCase());
+        
+        System.out.println(s.toUpperCase());
+
+        System.out.println(s.contains("rushi"));
+
+        System.out.println(s.startsWith("Aru"));
+
+        System.out.println(s.endsWith("Sharma"));
+
+
+
+    }
+}
