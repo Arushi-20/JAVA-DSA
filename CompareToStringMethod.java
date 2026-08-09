4113e3e101ebd70f921472f288ee6878d246ace1
Arushi Sharma <avpsharma3012@gmail.com>
2026-08-09 21:34:11 +0530

    CompareTo string method

---
+++ CompareToStringMethod.java
@@ -0,0 +1,7 @@
+public class CompareToStringMethod {
+    public static void main(String[] args) {
+        String s1="tea";
+        String s2= "ship";
+        System.out.println(s1.compareTo(s2));
+    }   
+}
