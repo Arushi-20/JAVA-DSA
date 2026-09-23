68d2c732e6c626372eee59118e63e58d8176fa83
Arushi Sharma <avpsharma3012@gmail.com>
2026-09-23 21:22:32 +0530

    Add files via upload

---
+++ MyQueue.java
@@ -0,0 +1,54 @@
+package Queues;
+
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
+    }
+}
+public class MyQueue {
+    Node head;
+    Node tail;
+    int size;
+    
+    void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    
+    int remove(){
+        if(size==0){
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int front = head.val;
+        head = head.next;
+        size--;
+        return front;
+    }
+
+    int peek(){
+        if(size==0){
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return head.val;
+    }
+
+
+    void add(int val){
+        Node temp = new Node(val);
+        if(size==0) head=tail=temp;
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+}
